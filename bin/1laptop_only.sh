fbcc1bad708200776ece02f35907d51c51d8bae3
Serge Kroes <serge@kroescontrol.nl>
2014-05-07 15:31:10 +0200

    1

--- bin/1laptop_only.sh
+++ bin/1laptop_only.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-xrandr --output LVDS-1 --mode 1600x900 --pos 0x0 --rotate normal --output DP-3 --off --output DP-2 --off --output DP-1 --off --output VGA-1 --off
+xrandr --output LVDS1 --mode 1366x768 --pos 0x0 --rotate normal --output DP-3 --off --output DP-2 --off --output DP1 --off --output VGA1 --off
 /opt/home/skroes/.i3/bin/i3_setbackground

4ebd7807032b3c411559b4c2e541af8af94efde0
Serge Kroes <serge@kroescontrol.nl>
2015-02-26 23:07:23 +0100

    cleanup files

--- bin/1laptop_only.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-xrandr --output LVDS1 --mode 1366x768 --pos 0x0 --rotate normal --output DP-3 --off --output DP-2 --off --output DP1 --off --output VGA1 --off
-/opt/home/skroes/.i3/bin/i3_setbackground
--- bin/3DP1_only.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-xrandr --output LVDS-1 --off --output DP-3 --off --output DP-2 --off --output DP-1 --auto --pos 0x0 --rotate normal --output VGA-1 --off
-/opt/home/skroes/.i3/bin/i3_setbackground

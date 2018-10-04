87830b0ceef679380c62cf780a2fed2ca8a21944
Serge Kroes <serge.kroes@crv4all.com>
2018-10-04 14:53:32 +0200

    i3status disable wrapper

--- bin/myi3status.sh
+++ bin/myi3status.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 # shell script to prepend i3status with more stuff
 
-i3status | while :
-do
-        read line
-        lala=`test -x /home/skroes/drive/notes/status.sh && /home/skroes/drive/notes/status.sh || echo "N"`
-        echo "$lala | $line" || exit 1
-done
+i3status
+# | while :
+#do
+#        read line
+#        lala=`test -x /home/skroes/drive/notes/status.sh && /home/skroes/drive/notes/status.sh || echo "N"`
+#        echo "$lala | $line" || exit 1
+#done

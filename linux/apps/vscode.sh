84cbb6fb0809ed3880809a93347a6bb73c286335
Serge Kroes <serge@kroescontrol.nl>
2018-12-22 23:36:11 +0100

    update run during code install

--- linux/apps/vscode.sh
+++ linux/apps/vscode.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -e
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+curl -s https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
 rm -f microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-sudo apt-get install apt-transport-https code -y
+sudo apt-get update -qq
+sudo apt-get install apt-transport-https code -qy
 # set as default editor
 sudo update-alternatives --set editor /usr/bin/code || true
 xdg-mime default code.desktop text/plain || true

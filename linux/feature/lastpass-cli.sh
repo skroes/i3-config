3491539eb4e887eee85aabae9b106524fb177061
Serge Kroes <serge.kroes@crv4all.com>
2019-01-02 10:36:21 +0100

    add libxslt1-dev and remove man page

--- linux/feature/lastpass-cli.sh
+++ linux/feature/lastpass-cli.sh
@@ -6,21 +6,23 @@ sudo apt-get --no-install-recommends -yqq install \
   cmake \
   libcurl4  \
   libcurl4-openssl-dev  \
+  libxslt1-dev \
   libssl-dev  \
   libxml2 \
   libxml2-dev  \
   libssl1.1 \
   pkg-config \
   ca-certificates \
-  xclip asciidoc xsltproc
-pwd
+  xclip \
+  asciidoc \
+  xsltproc
 
 cd tmp
 git clone https://github.com/lastpass/lastpass-cli.git || true 
 cd lastpass-cli
 make
 sudo make install
-sudo make install-doc
+#sudo make install-doc
 cd ..
 
 echo
@@ -28,4 +30,4 @@ echo "now login: lpass login x@c.x"
 echo
 echo "quick ref see: https://lastpass.github.io/lastpass-cli/lpass.1.html"
 echo
-lpass status | grep -i 'logged in as'
+#lpass status | grep -i 'logged in as'

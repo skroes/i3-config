a1262deaa3e11804f4aff3d3a7a53c4544cde26b
Serge Kroes <serge@kroescontrol.nl>
2019-01-03 20:44:29 +0100

    [alessio] [tag-fish/conf.d/oh_my_sergio.fish]

--- tag-fish/conf.d/oh_my_sergio.fish
+++ tag-fish/conf.d/oh_my_sergio.fish
@@ -2,9 +2,11 @@
 alias ssh-no-check='ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no'
 alias scp-no-check='scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no'
 
-set -g CDPATH ./ ~/workspace/crv/infra-devbox/environments/lab/modules/ ~/workspace/crv/infra-devbox/ ~/
+alias vi=code
+alias vim=code
 
-#alias kitchen='cd ~/workspace/crv/infra-devbox/ ;and /home/serkroes/workspace/crv/infra-devbox/vendor/bundle/ruby/2.5.0/bin/kitchen'
+set -g CDPATH ./ ~/workspace/crv/infra-devbox/environments/lab/modules/ ~/workspace/crv/infra-devbox/ ~/ 2>/dev/null
+
 # direnv if available
 if which direnv 2>&1 >/dev/null
   eval (direnv hook fish)

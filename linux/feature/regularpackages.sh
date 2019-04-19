1691ef738c601a87fb0b8a0c9f479a085c694dba
Serge Kroes <serge.kroes@crv4all.com>
2019-04-19 15:30:31 +0200

    [jules] [linux/feature/regularpackages.sh]

--- linux/feature/regularpackages.sh
+++ linux/feature/regularpackages.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -e
-sudo apt install stow direnv tig exfat-fuse/bionic solaar parcellite workrave -qqy 
+sudo apt install stow direnv tig exfat-fuse/bionic solaar parcellite workrave xautolock lxappearance gtk-chtheme qt4-qtconfig -qqy 

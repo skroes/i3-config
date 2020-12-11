8c2421c7cb11e398b42d5c1b5a362cd874ca53c6
Serge Kroes <serge@kroescontrol.nl>
2020-12-11 16:51:23 +0100

    hooa

--- linux/feature/regularpackages.sh
+++ linux/feature/regularpackages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 set -x
-sudo apt install curl git stow direnv tig solaar parcellite workrave xautolock lxappearance gtk-chtheme -qqy
+sudo apt install curl git stow direnv tig solaar parcellite workrave xautolock lxappearance gtk-chtheme ssh-askpass-gnome -qqy 
 # exfat-fuse/bionic 
 # qt4-qtconfig
